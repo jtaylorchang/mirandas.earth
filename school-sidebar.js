ee68bb1107d473fc64c8c7f730b5dc1bc45c909a
Jeff Taylor-Chang <jeff@jefftc.com>
2021-05-19 16:16:54 -0400

    Added ESE Capstone

--- school-sidebar.js
+++ school-sidebar.js
@@ -1,6 +1,8 @@
 module.exports = [
   "school/index",
   "school/nres-policy-memo",
+  "school/ese-environmental-writing",
+  "school/ese-capstone",
   "school/badm-canva-case",
   "school/badm-charity-miles-practice-case",
 ];
